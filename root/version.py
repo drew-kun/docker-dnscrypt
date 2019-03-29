8c5b6318853956cd2738c415070716a258f07592
RiX <rix1337@users.noreply.github.com>
2019-03-29 13:21:38 +0100

    Update version.py

--- root/version.py
+++ root/version.py
@@ -1,10 +1,9 @@
 import re
 import urllib2
 
-regex = r'<title>Release (\b.*) ·'
+regex = r'<title>Release ((\d+\.)?(\d+\.)?(\*|\d+))\s'
 
 SearchString = urllib2.urlopen('https://github.com/jedisct1/dnscrypt-proxy/releases/latest').read()
 
 matches = re.search(regex, SearchString)
-
 print matches.group(1)
