93475503fa0e9e32b91244b164684af6ca1f75a0
RiX <rix1337@users.noreply.github.com>
2019-10-16 09:09:24 +0200

    Fix broken update if Version contains "beta"

--- root/version.py
+++ root/version.py
@@ -1,7 +1,7 @@
 import re
 import urllib2
 
-regex = r'<title>Release ((\d+\.)?(\d+\.)?(\*|\d+))\s'
+regex = r'<title>Release ((\d+\.)?(\d+\.)?(\*|\d+.+?))\s'
 
 SearchString = urllib2.urlopen('https://github.com/jedisct1/dnscrypt-proxy/releases/latest').read()
 
